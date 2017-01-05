d484b426a99d7bc3c6e6899a591e02acbbc95404
Mims H. Wright <mims@mimswright.com>
2017-01-05 13:42:26 +0100

    Added test file. did some major work on the applicaiton of the styles to the specific syntaxes. more work yet to do. updated license and changelog.

---
+++ test.js
@@ -0,0 +1,32 @@
+class foo {
+  constructor () {
+    var a = "string";
+    const b = 123;
+    let c = (param) => {
+      return b;
+    }
+    var d = {
+      "name": "value"
+    };
+    var e = /regexp/g;
+    var f = [a, b, c, d, e];
+    var g = 0xFF00ff;
+    var h = "#ffffff"
+  },
+
+  abc: function () {
+    return this;
+  }
+}
+
+window.bar = "string";
+
+jsx = <test name="value">
+  Content
+</test>;
+
+// Comment
+
+/*
+  Block Comment
+*/
